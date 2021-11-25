da65553403bd3b232860773df3ec1882fd4b4424
Dominik Snopek <222572@edu.p.lodz.pl>
2021-11-25 14:00:44 +0100

    add subject View finished

--- lib/screens/homeScreens/actions/add_subject.dart
+++ lib/screens/homeScreens/actions/add_subject.dart
@@ -1,7 +1,8 @@
 import 'package:delta_squad_app/classes/subject.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:intl/intl.dart';
 
 class AddSubject extends StatefulWidget {
   const AddSubject({Key? key, required this.subjects}) : super(key: key);
@@ -12,6 +13,10 @@ class AddSubject extends StatefulWidget {
   _AddSubjectState createState() => _AddSubjectState();
 }
 
+enum Days { monday, tuesday, wednesday, thursday, friday, saturday, sunday }
+
+enum TimeType { all, x1, x2 }
+
 enum SingingCharacter { Lecture, Excercise, Laboratorium, Seminary }
 
 class _AddSubjectState extends State<AddSubject> {
@@ -19,16 +24,49 @@ class _AddSubjectState extends State<AddSubject> {
   late TextEditingController _teacherController;
   late TextEditingController _roomController;
 
+  Days? _days = Days.monday;
+  TimeType? _typy = TimeType.all;
+
+  late bool _switchValue;
+
+  late TextEditingController _beginTime;
+  late TextEditingController _endTime;
+  DateFormat dateFormat = DateFormat("HH:mm");
+
+  TimeOfDay _time = TimeOfDay(hour: 7, minute: 15);
+
   final _formKey = GlobalKey<FormState>();
 
   final List<bool> _subjectWeekValues = List.generate(15, (index) => false);
+
+  void _selectTime(TextEditingController txt) async {
+    final TimeOfDay? newTime = await showTimePicker(
+      context: context,
+      initialTime: TimeOfDay(hour: 7, minute: 15),
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child!,
+        );
+      },
+      initialEntryMode: TimePickerEntryMode.input,
+    );
+    setState(() {
+      final now = new DateTime.now();
+      var now2 =
+          DateTime(now.year, now.month, now.day, newTime!.hour, newTime.minute);
+      txt.text = dateFormat.format(now2);
+    });
+  }
 
   @override
   void initState() {
     _subjectController = TextEditingController();
     _teacherController = TextEditingController();
     _roomController = TextEditingController();
-
+    _beginTime = TextEditingController();
+    _endTime = TextEditingController();
+    _switchValue = false;
 
     super.initState();
   }
@@ -38,6 +76,8 @@ class _AddSubjectState extends State<AddSubject> {
     _subjectController.dispose();
     _teacherController.dispose();
     _roomController.dispose();
+    _beginTime.dispose();
+    _endTime.dispose();
 
     super.dispose();
   }
@@ -63,9 +103,9 @@ class _AddSubjectState extends State<AddSubject> {
                   TextFormField(
                     controller: _subjectController,
                     validator: (text) =>
-                      text!.length > 0 ? null : "Wprowadź nazwę przedmiotu",
+                        text!.length > 0 ? null : "Wprowadź nazwę przedmiotu",
                     decoration: InputDecoration(
-                        label: Text("Nazwa przedmiotu"),
+                        label: const Text("Nazwa przedmiotu"),
                         prefixIcon: Icon(Icons.short_text),
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(10))),
@@ -73,10 +113,11 @@ class _AddSubjectState extends State<AddSubject> {
                   SizedBox(height: 20),
                   TextFormField(
                     controller: _teacherController,
-                    validator: (text) =>
-                    text!.length > 0 ? null : "Wprowadź nazwisko osoby prowadzącej zajęcia",
+                    validator: (text) => text!.length > 0
+                        ? null
+                        : "Wprowadź nazwisko osoby prowadzącej zajęcia",
                     decoration: InputDecoration(
-                        label: Text("Prowadzący"),
+                        label: const Text("Prowadzący"),
                         prefixIcon: Icon(Icons.person),
                         border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(10))),
@@ -85,7 +126,7 @@ class _AddSubjectState extends State<AddSubject> {
                   TextFormField(
                     controller: _roomController,
                     validator: (text) =>
-                    text!.length > 0 ? null : "Wprowadź identyfikator sali",
+                        text!.length > 0 ? null : "Wprowadź identyfikator sali",
                     decoration: InputDecoration(
                         label: Text("Sala"),
                         prefixIcon: Icon(Icons.room),
@@ -142,37 +183,223 @@ class _AddSubjectState extends State<AddSubject> {
                     indent: 5,
                     endIndent: 5,
                   ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: (MediaQuery.of(context).size.width - 40) / 2,
+                            child: RadioListTile(
+                              dense: true,
+                              title: const Text('Poniedziałek'),
+                              value: Days.monday,
+                              groupValue: _days,
+                              onChanged: (Days? value) {
+                                setState(() {
+                                  _days = value;
+                                });
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            width: (MediaQuery.of(context).size.width - 40) / 2,
+                            child: RadioListTile(
+                              dense: true,
+                              title: const Text('Wtorek'),
+                              value: Days.tuesday,
+                              groupValue: _days,
+                              onChanged: (Days? value) {
+                                setState(() {
+                                  _days = value;
+                                });
+                              },
+                            ),
+                          )
+                        ],
+                      ), //monday,tuesday
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: (MediaQuery.of(context).size.width - 40) / 2,
+                            child: RadioListTile(
+                              dense: true,
+                              title: const Text('Sroda'),
+                              value: Days.wednesday,
+                              groupValue: _days,
+                              onChanged: (Days? value) {
+                                setState(() {
+                                  _days = value;
+                                });
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            width: (MediaQuery.of(context).size.width - 40) / 2,
+                            child: RadioListTile(
+                              dense: true,
+                              title: const Text('Czwartek'),
+                              value: Days.thursday,
+                              groupValue: _days,
+                              onChanged: (Days? value) {
+                                setState(() {
+                                  _days = value;
+                                });
+                              },
+                            ),
+                          )
+                        ],
+                      ), //wednesday,thursday
+                      Row(
+                        children: [
+                          SizedBox(
+                            width: (MediaQuery.of(context).size.width - 40) / 2,
+                            child: RadioListTile(
+                              dense: true,
+                              title: const Text('Piątek'),
+                              value: Days.friday,
+                              groupValue: _days,
+                              onChanged: (Days? value) {
+                                setState(() {
+                                  _days = value;
+                                });
+                              },
+                            ),
+                          ),
+                          SizedBox(
+                            width: (MediaQuery.of(context).size.width - 40) / 2,
+                            child: RadioListTile(
+                              dense: true,
+                              title: const Text('Sobota'),
+                              value: Days.saturday,
+                              groupValue: _days,
+                              onChanged: (Days? value) {
+                                setState(() {
+                                  _days = value;
+                                });
+                              },
+                            ),
+                          )
+                        ],
+                      ), //friday,saturday
+                      Row(children: [
+                        SizedBox(
+                          width: (MediaQuery.of(context).size.width - 40) / 2,
+                          child: RadioListTile(
+                            dense: true,
+                            title: const Text('Niedziela'),
+                            value: Days.sunday,
+                            groupValue: _days,
+                            onChanged: (Days? value) {
+                              setState(() {
+                                _days = value;
+                              });
+                            },
+                          ),
+                        )
+                      ]),
+                    ],
+                  ),
+                  //sunday
+
+                  const Divider(
+                    color: Colors.black,
+                    height: 25,
+                    thickness: 0.5,
+                    indent: 5,
+                    endIndent: 5,
+                  ),
                   Row(
                     children: [
                       Container(
-                          width: MediaQuery.of(context).size.width * 0.20,
-                          child:Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              RadioListTile(
-                                  value: 1,
-                                  groupValue: val,
-                                  onChanged: (value) {
-                                    setState(() {
+                          width: MediaQuery.of(context).size.width - 40,
+                          child: SwitchListTile(
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal:
+                                    (MediaQuery.of(context).size.width / 2) -
+                                        50),
+                            value: _switchValue,
+                            activeColor: Colors.blue,
+                            onChanged: (bool value) {
+                              setState(() {
+                                _switchValue = !_switchValue;
+                              });
+                            },
+                          )),
+                    ],
+                  ),
 
-                                    });
-                                  }),
-                            ],
-                          )
-                      ),
-                      Container(
-                          width: MediaQuery.of(context).size.width * 0.69,
-                          child:Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                  Row(
+                    children: [
+                      if (_switchValue == true)
+                        Container(
+                            width: MediaQuery.of(context).size.width - 40,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                _buildCheckBoxes(1, 3, _subjectWeekValues),
+                                _buildCheckBoxes(4, 6, _subjectWeekValues),
+                                _buildCheckBoxes(7, 9, _subjectWeekValues),
+                                _buildCheckBoxes(10, 12, _subjectWeekValues),
+                                _buildCheckBoxes(13, 15, _subjectWeekValues),
+                              ],
+                            )),
+                      if (_switchValue == false)
+                        Container(
+                          width: MediaQuery.of(context).size.width - 40,
+                          child: Column(
                             children: [
-                              _buildCheckBoxes(1, 3, _subjectWeekValues),
-                              _buildCheckBoxes(4, 6, _subjectWeekValues),
-                              _buildCheckBoxes(7, 9, _subjectWeekValues),
-                              _buildCheckBoxes(10, 12, _subjectWeekValues),
-                              _buildCheckBoxes(13, 15, _subjectWeekValues),
+                              Row(
+                                children: [
+                                  SizedBox(
+                                    width: (MediaQuery.of(context).size.width -
+                                            40) /
+                                        3,
+                                    child: RadioListTile(
+                                      title: const Text('ALL'),
+                                      value: TimeType.all,
+                                      groupValue: _typy,
+                                      onChanged: (TimeType? value) {
+                                        setState(() {
+                                          _typy = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: (MediaQuery.of(context).size.width -
+                                            40) /
+                                        3,
+                                    child: RadioListTile(
+                                      title: const Text('X1'),
+                                      value: TimeType.x1,
+                                      groupValue: _typy,
+                                      onChanged: (TimeType? value) {
+                                        setState(() {
+                                          _typy = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  SizedBox(
+                                    width: (MediaQuery.of(context).size.width -
+                                            40) /
+                                        3,
+                                    child: RadioListTile(
+                                      title: const Text('X2'),
+                                      value: TimeType.x2,
+                                      groupValue: _typy,
+                                      onChanged: (TimeType? value) {
+                                        setState(() {
+                                          _typy = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ],
-                          )
-                      ),
+                          ),
+                        ),
                     ],
                   ),
                   SizedBox(height: 20),
@@ -186,7 +413,7 @@ class _AddSubjectState extends State<AddSubject> {
                     ),
                     child: const Text('Dodaj'),
                     onPressed: () {
-                      if(_formKey.currentState!.validate()) {
+                      if (_formKey.currentState!.validate()) {
                         addSubject();
                         Navigator.pop(context);
                       }
@@ -202,15 +429,13 @@ class _AddSubjectState extends State<AddSubject> {
   }
 
   addSubject() {
-
-
     final DateTime today = DateTime.now();
     DateTime startTime = DateTime(today.year, today.month, today.day, 8, 0, 0);
     DateTime endTime = startTime.add(const Duration(hours: 2));
 
     Color color;
 
-    switch(_character) {
+    switch (_character) {
       case SingingCharacter.Lecture:
         color = Colors.orange;
         break;
@@ -227,17 +452,23 @@ class _AddSubjectState extends State<AddSubject> {
         color = Colors.lime;
     }
 
-    widget.subjects.add(Subject('${_subjectController.text}\n${_teacherController.text}\n${_roomController.text}', startTime, endTime, color, false));
+    widget.subjects.add(Subject(
+        '${_subjectController.text}\n${_teacherController.text}\n${_roomController.text}',
+        startTime,
+        endTime,
+        color,
+        false));
   }
 
   Widget _buildCheckBoxes(int start, int end, List<bool> values) {
     List<Widget> list = [];
-    Widget cb,ca;
+    Widget cb, ca;
     Widget ex;
 
-    for (int i=start;i<=end;++i) {
+    for (int i = start; i <= end; ++i) {
       cb = CheckboxListTile(
-        contentPadding: EdgeInsets.symmetric(horizontal: 10),
+          dense: true,
+          contentPadding: EdgeInsets.symmetric(horizontal: 10),
           title: Text(i.toString()),
           value: values[i - 1],
           onChanged: (value) {
@@ -246,21 +477,19 @@ class _AddSubjectState extends State<AddSubject> {
             });
           });
       ca = ListTile(
+        dense: true,
         leading: Checkbox(
           value: false,
-          onChanged: (bool? value) {  },
+          onChanged: (bool? value) {},
         ),
         title: Text(i.toString()),
       );
       ex = Expanded(
-        child: cb,
+        child: ca,
       );
       list.add(ex);
     }
     print(list);
-    return Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: list
-    );
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: list);
   }
 }
