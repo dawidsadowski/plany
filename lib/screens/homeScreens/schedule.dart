f42dde0aba45b3163681a1bb26779346c8ea9a5a
dawidsadowski <dsadowski@craftware.biz>
2021-11-26 13:55:03 +0100

    Add Schedule Menu Drawer view

--- lib/screens/homeScreens/schedule.dart
+++ lib/screens/homeScreens/schedule.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:delta_squad_app/classes/subject.dart';
 import 'package:delta_squad_app/screens/homeScreens/actions/add_subject.dart';
 import 'package:flutter/material.dart';
@@ -11,9 +13,8 @@ class Schedule extends StatefulWidget {
 }
 
 class _ScheduleState extends State<Schedule> {
+  int _selectedDestination = 0;
   List<Subject> subjects = <Subject>[];
-  bool _menuOpened = false;
-
   CalendarTapDetails? _details;
 
   @override
@@ -21,16 +22,6 @@ class _ScheduleState extends State<Schedule> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Plan zajęć"),
-        leading: GestureDetector(
-            onTap: () {},
-            child: IconButton(
-                splashRadius: 20,
-                onPressed: () {
-                  setState(() {
-                    _menuOpened = !_menuOpened;
-                  });
-                },
-                icon: Icon(_menuOpened ? Icons.arrow_back : Icons.menu))),
         actions: [
           GestureDetector(
             onTap: () {},
@@ -54,11 +45,111 @@ class _ScheduleState extends State<Schedule> {
           ),
         ],
       ),
+      drawer: Drawer(
+          child: ListView(children: [
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            "Delta Szwadron",
+            style: TextStyle(
+              fontFeatures: [FontFeature.enable('smcp')],
+              fontSize: 25,
+              color: Colors.black54,
+            ),
+          ),
+        ),
+        const Divider(
+          height: 1,
+          thickness: 1,
+        ),
+        ListTile(
+          leading: Icon(Icons.calendar_view_day),
+          title: Text('Dzień'),
+          selected: _selectedDestination == 0,
+          onTap: () => selectDestination(0),
+        ),
+        ListTile(
+          leading: Icon(Icons.calendar_view_week),
+          title: Text('Tydzień'),
+          selected: _selectedDestination == 1,
+          onTap: () => selectDestination(1),
+        ),
+        ListTile(
+          leading: Icon(Icons.calendar_view_month),
+          title: Text('Miesiąc'),
+          selected: _selectedDestination == 2,
+          onTap: () => selectDestination(2),
+        ),
+        const Divider(
+          height: 1,
+          thickness: 1,
+        ),
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Grupy',
+          ),
+        ),
+        CheckboxListTile(
+            title: Text("TM6"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: true,
+            onChanged: (value) {}),
+        CheckboxListTile(
+            title: Text("TIZJO1"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: true,
+            onChanged: (value) {}),
+        const Divider(
+          height: 1,
+          thickness: 1,
+        ),
+        const Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Text(
+            'Rodzaj zajęć',
+          ),
+        ),
+        CheckboxListTile(
+            activeColor: Colors.orange,
+            title: Text("Wykłady"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: true,
+            onChanged: (value) {}),
+        CheckboxListTile(
+            activeColor: Colors.teal,
+            title: Text("Ćwiczenia"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: true,
+            onChanged: (value) {}),
+        CheckboxListTile(
+            activeColor: Colors.blue,
+            title: Text("Laboratoria"),
+            controlAffinity: ListTileControlAffinity.leading,
+            value: true,
+            onChanged: (value) {}),
+            CheckboxListTile(
+                activeColor: Colors.redAccent,
+                title: Text("Seminaria"),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: true,
+                onChanged: (value) {}),
+            CheckboxListTile(
+                activeColor: Colors.black54,
+                title: Text("Inne"),
+                controlAffinity: ListTileControlAffinity.leading,
+                value: true,
+                onChanged: (value) {}),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           await Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => AddSubject(subjects: subjects, details: _details)),
+            MaterialPageRoute(
+                builder: (context) =>
+                    AddSubject(subjects: subjects, details: _details)),
           );
 
           setState(() {});
@@ -75,14 +166,20 @@ class _ScheduleState extends State<Schedule> {
           view: CalendarView.day,
           dataSource: MeetingDataSource(subjects),
           timeSlotViewSettings: const TimeSlotViewSettings(
-              timeFormat: "HH:mm",
-              // startHour: 0,
-              // endHour: 23,
-              // nonWorkingDays: <int>[DateTime.friday, DateTime.saturday]
+            timeFormat: "HH:mm",
+            // startHour: 0,
+            // endHour: 23,
+            // nonWorkingDays: <int>[DateTime.friday, DateTime.saturday]
           ),
         ),
       ),
     );
+  }
+
+  void selectDestination(int index) {
+    setState(() {
+      _selectedDestination = index;
+    });
   }
 }
 
@@ -126,5 +223,3 @@ class MeetingDataSource extends CalendarDataSource {
     return meetingData;
   }
 }
-
-
